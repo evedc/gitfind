a0b1fdcfb00ee9b6d1f6108b2fe8ccae90a29e15
Évelim Dornelles <dorrrnelles@gmail.com>
2024-10-22 11:00:57 -0300

    upload

--- gitfind/src/pages/Home/index.js
+++ gitfind/src/pages/Home/index.js
@@ -1,9 +1,15 @@
 import { Header } from '../../components/Header';
+import background from '../../assets/background.png';
+
+import './styles.css'
 
 function App() {
     return (
         <div className="App">
             <Header />
+            <div className="conteudo">
+                <img src={background} className="background" alt="background app" />
+            </div>
         </div>
     );
 }
